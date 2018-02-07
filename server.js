9efb296d82c920fb9b9610284f5038c914de7ba0
YulijaU <cenite-moment@mail.ru>
2018-02-07 13:57:20 +0700

    Tweaks a directory structure and init React and babel

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var path = require('path');
 var port = 3000;
 var app = express();
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../src/index.html'));
-});
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../dist/index.html'));
+// });
+
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(port, function(err) {
     if(err) {
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.React = React;
+
+ReactDOM.render(
+    <h1>Markdown Previewer</h1>, 
+    document.getElementById('root')
+);
